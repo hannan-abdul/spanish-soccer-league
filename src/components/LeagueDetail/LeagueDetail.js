68278db4bbb38a7ac4c1e5782bfc16e0ea6c8e89
hannan-abdul <shohas563@gmail.com>
2021-03-11 17:35:14 +0600

    conditional updated

--- src/components/LeagueDetail/LeagueDetail.js
+++ src/components/LeagueDetail/LeagueDetail.js
@@ -3,42 +3,46 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFlag, faFutbol, faMapMarkerAlt, faMars } from '@fortawesome/free-solid-svg-icons';
 import { useParams } from 'react-router';
 import maleImage from '../../images/Photo/male.png';
-import './LeagueDetail.css'
+import femaleImage from '../../images/Photo/female.png';
+import './LeagueDetail.css';
 import SocialIcon from '../SocialIcon/SocialIcon';
 
 const LeagueDetail = () => {
     const { idTeam } = useParams();
     const [league, setLeague] = useState({})
-    
+    const {intFormedYear, strCountry, strSport, strGender, strTeamBadge, strLeague,} = league;
     useEffect(() => {
         const url = `https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=${idTeam}`;
         fetch(url)
             .then(res => res.json())
             .then(data => setLeague(data.teams[0]))
     }, [idTeam])
-
-    const {intFormedYear, strCountry, strSport, strGender} = league;
+    
+    let image;
+    if (strGender === "Male"){
+        image = <img className='gender-image' src={maleImage} alt="male"/>
+    }
+    else{
+        image = <img className='gender-image' src={femaleImage} alt="female"/>
+    }
 
     return (
         <div className="container">
             <div className="header-fix">
                 <div className="logo-position">
-                    <img src={league.strTeamBadge} alt="banner"/>
+                    <img src={strTeamBadge} alt="badge"/>
                 </div>
             </div>
             <div className="row team-details-row">
                 <div className="col-lg-6 col-md-6 col-sm-12 team-details">
-                    <h2>{league.strLeague}</h2>
+                    <h2>{strLeague}</h2>
                     <h6><FontAwesomeIcon icon={faMapMarkerAlt} /> Founded: {intFormedYear}</h6>
                     <h6><FontAwesomeIcon icon={faFlag} /> Country: {strCountry}</h6>
                     <h6><FontAwesomeIcon icon={faFutbol} /> Sports Type: {strSport}</h6>
                     <h6><FontAwesomeIcon icon={faMars} /> Gender: {strGender}</h6>
                 </div>
                 <div className="col-lg-6 col-md-6 col-sm-12">
-                    {/* if({league.strGender} === male){
-                        <img className='gender-image' src={maleImage} alt=""/>
-                    } */}
-                    <img className='gender-image' src={maleImage} alt=""/>
+                    {image}
                 </div>
             </div>
             <div className="more-details">
